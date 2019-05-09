e2ce314cdf278a868eb85e62f113debafbcc1039
Soulike <316413310@qq.com>
2019-05-09 11:03:10 +0800

    页面框架

---
+++ src/Pages/EventPage/Container.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import View from './View';
+
+class EventPage extends React.Component
+{
+    render()
+    {
+        return (
+            <View />
+        );
+    }
+}
+
+export default EventPage;
---
+++ src/Pages/EventPage/View.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Style from './Style.module.scss';
+
+function EventPage(props)
+{
+    return (
+        <div className={Style.EventPage}>
+
+        </div>
+    );
+}
+
+export default React.memo(EventPage);
