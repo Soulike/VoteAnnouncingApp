8f5b893f4401047bd062429e2e9e80fc81b7853f
Soulike <316413310@qq.com>
2019-05-09 11:24:23 +0800

    TopBar 框架

---
+++ src/Pages/EventPage/Components/TopBar/View.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Style from './Style.module.scss';
+
+function TopBarView()
+{
+    return (
+        <div className={Style.TopBar}>
+
+        </div>
+    );
+}
+
+export default React.memo(TopBarView);
