f3c77b1ecd4a8ee5b4d317e6a50d21fbd8d9f984
Soulike <316413310@qq.com>
2019-05-09 19:19:47 +0800

    Updates

---
+++ precache-manifest.909987d713de8a515f5a5b0cfae9878c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22730aa92c8501a41a398d938358e7d",
+    "url": "/VoteAnnouncingApp/index.html"
+  },
+  {
+    "revision": "b24b4cf1561b70846cda",
+    "url": "/VoteAnnouncingApp/static/css/2.c59793d0.chunk.css"
+  },
+  {
+    "revision": "e7d74b727e61985f1a85",
+    "url": "/VoteAnnouncingApp/static/css/main.2c3aa5a3.chunk.css"
+  },
+  {
+    "revision": "b24b4cf1561b70846cda",
+    "url": "/VoteAnnouncingApp/static/js/2.a0a3e90c.chunk.js"
+  },
+  {
+    "revision": "e7d74b727e61985f1a85",
+    "url": "/VoteAnnouncingApp/static/js/main.e61e0bb1.chunk.js"
+  },
+  {
+    "revision": "9ccc8874c109e9da77b6",
+    "url": "/VoteAnnouncingApp/static/js/runtime~main.65c36078.js"
+  }
+]);
